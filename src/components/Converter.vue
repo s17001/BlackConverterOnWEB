79fccd5821aec6143e49b029c4b7579b6f1b816a
s17001 <s17001@std.it-college.ac.jp>
2019-06-17 14:56:32 +0900

    基礎デザインを作った

--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <p>this is converter</p>
+        <div>ここは説明文です</div>
+        <div><textarea name="" id="input" cols="30" rows="10"></textarea></div>
+        <div><textarea name="" id="output" cols="30" rows="10"></textarea></div>
     </div>
 </template>
 
