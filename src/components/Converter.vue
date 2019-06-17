4b83d403005ac708f9fcf93d5369c4bf9693eb5b
s17001 <s17001@std.it-college.ac.jp>
2019-06-17 15:01:53 +0900

    script内部の下味を整えた

--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -1,14 +1,23 @@
 <template>
     <div>
         <div>ここは説明文です</div>
-        <div><textarea name="" id="input" cols="30" rows="10"></textarea></div>
+        <div><textarea v-model="input" onchange="" name="" id="input" cols="30" rows="10"></textarea></div>
         <div><textarea name="" id="output" cols="30" rows="10"></textarea></div>
+        <p>{{input}}</p>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Converter"
+        name: "Converter",
+        data() {
+            return {
+                input:''
+            }
+        },
+        methods:{
+
+        }
     }
 </script>
 
